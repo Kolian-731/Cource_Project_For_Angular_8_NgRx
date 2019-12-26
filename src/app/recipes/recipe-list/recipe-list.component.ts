ae4cf44568e797ffe967e07ab2496ceecdc453c2
Kolian Karapoziuk <kolian55544@gmail.com>
2019-12-26 17:48:50 +0200

    109lesson

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
+import { RecipeService } from '../recipe.service';
+
 @Component({
   selector: "app-recipe-list",
   templateUrl: "./recipe-list.component.html",
@@ -7,28 +9,13 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelcted = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe(
-      "A test Recipe",
-      "This Is simply a test",
-      "https://cdn.apartmenttherapy.info/image/upload/v1564775676/k/Photo/Recipes/2019-08-how-to-juiciest-turkey-meatballs/How-to-Make-the-Best-Juiciest-Turkey-Meatballs_055.jpg"
-    ),
-    new Recipe(
-      "A test Recipe",
-      "This Is simply a test",
-      "https://cookieandkate.com/images/2019/02/best-shakshuka-recipe-3-768x1154.jpg"
-    ),
+  recipes: Recipe[];
 
-    new Recipe(
-      "A test Recipe",
-      "This Is simply a test",
-      "https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13.jpg"
-    )
-  ];
+  constructor(private recipeService: RecipeService) { }
 
-  constructor() { }
-
-  ngOnInit() { }
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelcted.emit(recipe);
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,26 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A test Recipe",
+      "This Is simply a test",
+      "https://cdn.apartmenttherapy.info/image/upload/v1564775676/k/Photo/Recipes/2019-08-how-to-juiciest-turkey-meatballs/How-to-Make-the-Best-Juiciest-Turkey-Meatballs_055.jpg"
+    ),
+    new Recipe(
+      "A test Recipe",
+      "This Is simply a test",
+      "https://cookieandkate.com/images/2019/02/best-shakshuka-recipe-3-768x1154.jpg"
+    ),
+
+    new Recipe(
+      "A test Recipe",
+      "This Is simply a test",
+      "https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13.jpg"
+    )
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
