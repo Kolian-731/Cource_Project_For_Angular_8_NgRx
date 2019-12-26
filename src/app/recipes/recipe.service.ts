8ca66e4f1f0516248639ad60fce1708bad22a3f4
Kolian Karapoziuk <kolian55544@gmail.com>
2019-12-26 18:04:03 +0200

    109lesson

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,10 @@
+import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
+
+  resipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe(
       "A test Recipe",
